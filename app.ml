571815922591dd7dbe2083b7d94c5614c6ae1cef
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-04-12 00:14:17 +0900

    debug

---
+++ app.ml
@@ -0,0 +1,8 @@
+let () =
+  Dream.run ~debug:true @@ Dream.logger
+  @@ Dream.router
+       [
+         Dream.get "/bad" (fun _ -> Dream.empty `Bad_Request);
+         Dream.get "/fail" (fun _ -> raise (Failure "The Web app failed!"));
+       ]
+  @@ Dream.not_found
