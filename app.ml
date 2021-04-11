99715d175fb657de428ef425d55be03e63f1c914
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-04-12 00:43:01 +0900

    use Tyxml

--- app.ml
+++ app.ml
@@ -1,12 +1,16 @@
+let render path_param =
+  let open Tyxml.Html in
+  html (head (title (txt "Home")) []) (body [ h1 [ txt path_param ] ])
+
+let html_to_string html = Format.asprintf "%a" (Tyxml.Html.pp ()) html
+
 let () =
   Dream.run @@ Dream.logger
   @@ Dream.router
        [
-         Dream.get "/" (fun request ->
-             Dream.log "Sending greeting to %s!" (Dream.client request);
-             Dream.respond "Good morning, world!");
-         Dream.get "/fail" (fun _ ->
-             Dream.warning (fun log -> log "Raising an exception!");
-             raise (Failure "The Web app failed!"));
+         Dream.get "/" (fun _ -> Dream.respond "Hello, world!");
+         Dream.get "/:word" (fun request ->
+             render (Dream.param "word" request)
+             |> html_to_string |> Dream.respond);
        ]
   @@ Dream.not_found
