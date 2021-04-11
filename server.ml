cea933e24e50c48475c7b40836c422217d991d1c
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-04-11 23:47:48 +0900

    echo

--- server.ml
+++ server.ml
@@ -1,25 +1,9 @@
-let successful = ref 0
-
-let failed = ref 0
-
-let count_requests inner_handler request =
-  try%lwt
-    let%lwt response = inner_handler request in
-    successful := !successful + 1;
-    Lwt.return response
-  with exn ->
-    failed := !failed + 1;
-    raise exn
-
 let () =
-  Dream.run @@ Dream.logger @@ count_requests
+  Dream.run @@ Dream.logger
   @@ Dream.router
        [
-         Dream.get "/fail" (fun _ -> raise (Failure "The Web app failed!"));
-         Dream.get "/" (fun _ ->
-             Dream.respond
-               (Printf.sprintf
-                  "%3i request(s) successful\n%3i request(s) failed" !successful
-                  !failed));
+         Dream.post "/echo" (fun request ->
+             let%lwt body = Dream.body request in
+             Dream.respond (body ^ " hoge"));
        ]
   @@ Dream.not_found
