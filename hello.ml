3fa712fa8324aa7258069c2788e6d3e9ed51c1d4
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-04-11 22:48:47 +0900

    add router

--- hello.ml
+++ hello.ml
@@ -1,2 +1,9 @@
 let () =
-  Dream.run @@ Dream.logger @@ fun _ -> Dream.respond "Hello, world!"
+  Dream.run @@ Dream.logger
+  @@ Dream.router
+       [
+         Dream.get "/" (fun _ -> Dream.respond "Hello, world!");
+         Dream.get "/echo/:word" (fun request ->
+             Dream.respond (Dream.param "word" request));
+       ]
+  @@ Dream.not_found
